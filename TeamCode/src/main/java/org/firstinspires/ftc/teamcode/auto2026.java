b787a04fa157f82c12694c7634d6c4037053443d
winac2 <136137968+winac2@users.noreply.github.com>
2026-01-27 20:13:16 +0700

    PID rotate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto2026.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto2026.java
@@ -1,93 +1,133 @@
 package org.firstinspires.ftc.teamcode;
 //up update
-import com.qualcomm.hardware.bosch.BNO055IMU;
+//import static androidx.core.math.MathUtils.clamp;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.SensorBNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-
+@Autonomous(name="auto2026")
 public class auto2026 extends LinearOpMode {
 
 //    DcMotorEx motor;
-    DcMotorEx left;
-    DcMotorEx right;
+//    public DcMotor DTLeftMotor, DTRightMotor, Intake1, Intake2, Intake3, Intake4, Outtake1, Outtake2;
 
-    private BNO055IMU imu;
+    DcMotor DTLeftMotor, DTRightMotor;
+    public IMU imu;
 
+
+    //==PID_config==
     double integralSum = 0;
-    double Kp = 0;
-    double Ki = 0;
-    double Kd = 0;
+    double Kp = 2.0;  //quay nhanh, nhung khong bi overshoot
+    double Ki = 0.0;    //tranh tinh trang overshoot
+    double Kd = 0.2;    //phanh muot hon
 //    double Kf = 10;
 
     ElapsedTime timer = new ElapsedTime();
-    private double lastError = 0;
+    double lastError = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 //        motor = hardwareMap.get(DcMotorEx.class, "motor");
 //        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        right = hardwareMap.get(DcMotorEx.class, "right");
-        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        left = hardwareMap.get(DcMotorEx.class, "left");
-        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        DTRightMotor = hardwareMap.get(DcMotor.class, "DTRightMotor");
+        DTRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        DTLeftMotor = hardwareMap.get(DcMotor.class, "DTLeftMotor");
+        DTLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
-        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        DTLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
 
-        imu = hardwareMap.get(BNO055IMU.class,"imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        imu.initialize(parameters);
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot orientationOnRobot =
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+                );
+
+        IMU.Parameters imuParameters = new IMU.Parameters(orientationOnRobot);
+        imu.initialize(imuParameters);
+        imu.resetYaw();
 
         waitForStart();
 
-        double referenceAngle = Math.toRadians(90);
+        double targetAngle = Math.toRadians(90);
+        timer.reset();
+
         while (opModeIsActive()) {
-            double power = PIDControl(referenceAngle, imu.getAngularOrientation().firstAngle);
-            left.setPower(power);
-            right.setPower(-power);
+
+            //==execute== arms bu cac cho
+            double currentAngle = getYaw();
+            double power = PIDControl(targetAngle, currentAngle);
+
+            DTLeftMotor.setPower(power);
+            DTLeftMotor.setPower(-power);
+
+
+            telemetry.addData("Target (deg)", 90);
+            telemetry.addData("Current (deg)", Math.toDegrees(currentAngle));
+            telemetry.addData("Error (deg)", Math.toDegrees(targetAngle - currentAngle));
+            telemetry.update();
+
         }
     }
 
+    //===every another stupid AF functions===
 
+
+    //PID
     public double PIDControl(double reference, double state){
         double error = angleWrap(reference - state);
-        integralSum += error * timer.seconds();
-        double derivative = (error - lastError) / timer.seconds();
+
+        double dt = timer.seconds();
+        timer.reset();
+
+        integralSum += error * dt;
+        integralSum = clamp(integralSum, -0.4, 0.4);
+
+        double derivative = (error - lastError) / dt;
         lastError = error;
 
 
-        timer.reset();
-
         double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki);
 //        double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki) + (reference * Kf); //Feedforward: predict target dieu chinh toc do nhanh hon
-        return output;
+        return clamp(output, -1, 1);
     }
 
-    public double angleWrap(double radians){
-        while (radians > Math.PI){
-            radians -= 2 * Math.PI;
-        }
-        while (radians < -Math.PI){
-            radians += 2 * Math.PI;
-        }
-        return  radians;
+
+    // ===== IMU YAW =====
+    public double getYaw() {
+        YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+        return angles.getYaw(AngleUnit.RADIANS);
     }
+
+    // ===== UTILS =====
+    public double angleWrap(double radians) {
+        while (radians > Math.PI) radians -= 2 * Math.PI;
+        while (radians < -Math.PI) radians += 2 * Math.PI;
+        return radians;
+    }
+
+    public double clamp(double val, double min, double max) {
+        return Math.max(min, Math.min(max, val));
+    }
+
+
     public void drive(double power_left, double power_right, long time){
-        left.setPower(power_left);
-        right.setPower(power_right);
+        DTLeftMotor.setPower(power_left);
+        DTRightMotor.setPower(power_right);
         sleep(time);
-        left.setPower(0);
-        right.setPower(0);
+        DTLeftMotor.setPower(0);
+        DTRightMotor.setPower(0);
     }
 
 }
